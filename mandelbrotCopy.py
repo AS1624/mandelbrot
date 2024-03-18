c46578cd52a954352db51bad1245ef46a70bc968
AS1624 <thebest1dudes@gmail.com>
2024-03-18 14:10:08 -0800

    school

--- mandelbrotCopy.py
+++ mandelbrotCopy.py
@@ -5,16 +5,21 @@ import math
 import threading
 
 
-width = 200
+width = 600
 height = width
 scale = 1
-wheelCount = 0
-winScale = math.floor(1000 / width)
-maxReps = 30
+wheelCount = -20
+winScale = math.floor(600 / width)
+maxReps = 70
 dataType = np.complex128
 
-transX = 0
-transY = 0
+transX = -0.15748662592470025
+transY = 1.0252715546584952
+
+explore = False
+min = -20
+step = 0.5
+max = 170
 
 halfW = width / 2
 halfH = height / 2
@@ -28,10 +33,13 @@ def constrain(val, min_val, max_val):
 
     return val
 def col(mag):
-    return (255 - mag, 128, 0)
+#    print(mag)
+    mag = math.sqrt(mag / maxReps) * 255
+    # return (mag, mag, mag)
+    return mag
 
 def create(name):
-    pixels = np.zeros((width, height), dtype=np.integer)
+    pixels = np.full((width, height), 0, dtype=np.uint8)
 
     Z = np.zeros((width, height), dtype=dataType)
 
@@ -52,66 +60,85 @@ def create(name):
     M = np.full((width, height), True, dtype=bool)
 
     counts = np.zeros(maxReps)
+    total = 0
 
-    for i in range(maxReps):
+    for i in range(maxReps): # pass 1
         Z[M] = Z[M] * Z[M] + C[M]
-        overWrite = M + np.abs(Z) > 2
-        pixels[overWrite] = i
+        overWrite = np.logical_and(M, np.abs(Z) > 2)
+        pixels[overWrite] = col(i)
         M[np.abs(Z) > 2] = False
-    
-    for y in range(len(pixels)):
+    '''    
+    for y in range(len(pixels)): # pass 2
         for x in range(len(pixels[y])):
             counts[pixels[y, x]] += 1
     
+    for i in range(maxReps): # pass 3
+        total += counts[i]
 
-    for y in range(len(pixels)):
-        for x in range(len(pixels[y])):
-            pygame.draw.rect(win, (col(0) if M[y, x] else col(255)), (x * winScale, y * winScale, winScale, winScale))
+    for y in range(len(pixels)): # pass 4
+        for x in range(len(pixels[0])):
+            iterations = pixels[y, x]
+            pixels[y, x] = 0
+            for i in range(iterations + 1):
+                pixels[y, x] += counts[i] / total
+   '''
+    if not explore:
+        png.from_array(pixels, 'L').save(name)
+    if explore:
+        for y in range(len(pixels)):
+            for x in range(len(pixels[y])):
+                pygame.draw.rect(win, col(pixels[y, x]), (x * winScale, y * winScale, winScale, winScale))
+if explore:
+    pygame.init()
 
-
-pygame.init()
-
-win = pygame.display.set_mode((width * winScale, height * winScale))
-clock = pygame.time.Clock()
+    win = pygame.display.set_mode((width * winScale, height * winScale))
+    clock = pygame.time.Clock()
 
 running = True
 while running:
-    print(maxReps, clock.get_fps())
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEWHEEL:
-            wheelCount += event.y
-            scale = math.exp(wheelCount / 10)
+    if not explore:
+        print("wheelCount: ", wheelCount)
+    if explore:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEWHEEL:
+                wheelCount += event.y
+        x, y = (0, 0)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x -= 20 / clock.get_fps()
+        if keys[pygame.K_RIGHT]:
+            x += 20 / clock.get_fps()
+        if keys[pygame.K_UP]:
+            y -= 20 / clock.get_fps()
+        if keys[pygame.K_DOWN]:
+            y += 20 / clock.get_fps()
+        if keys[pygame.K_EQUALS]:
+            maxReps += 1
+        if keys[pygame.K_MINUS]:
+            maxReps -= 1
 
-            xDenom = 1 / (width * scale)
-            yDenom = 1 / (height * scale)
     x, y = (0, 0)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= 2
-    if keys[pygame.K_RIGHT]:
-        x += 2
-    if keys[pygame.K_UP]:
-        y -= 2
-    if keys[pygame.K_DOWN]:
-        y += 2
-    if keys[pygame.K_EQUALS]:
-        maxReps += 1
-    if keys[pygame.K_MINUS]:
-        maxReps -= 1
-
-        
     transX += x / (width ) / scale
     transY += y / (height ) / scale
-    # print(transX, transY)
+    scale = math.exp(wheelCount / 10)
+
+    xDenom = 1 / (width * scale)
+    yDenom = 1 / (height * scale)
 
     halfW = width / 2
     halfH = height / 2
     
-    create("")
-    pygame.draw.line(win, 0x888888, (width * winScale / 2, 0), (width * winScale / 2, height * winScale))
-    pygame.draw.line(win, 0x888888, (0, height * winScale / 2), ( width * winScale, height * winScale / 2))
+    create("movie/{}.png".format(int( (wheelCount - min ) / step) ))
+    if not explore:
+        wheelCount += step
+        if wheelCount > max:
+            running = False
+    if explore:
+        pygame.draw.line(win, 0x888888, (width * winScale / 2, 0), (width * winScale / 2, height * winScale))
+        pygame.draw.line(win, 0x888888, (0, height * winScale / 2), ( width * winScale, height * winScale / 2))
 
-    pygame.display.update()
+        pygame.display.update()
+# create("test.png")

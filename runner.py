d546691979130788935c310ae26cf2a130146ad0
AS1624 <thebest1dudes@gmail.com>
2024-03-24 14:16:52 -0800

    add runner

---
+++ runner.py
@@ -0,0 +1,33 @@
+import os
+import math
+import psutil
+
+min = -20
+max = 300
+step = 0.5
+
+max_processes = 4
+
+transX = -0.6373537012997929
+transY = 0.40434321082891456
+maxReps = 334
+
+def processes():
+    running = psutil.process_iter(attrs=['pid', 'name', 'status'])
+    
+    return sum(1 for p in running if p.info['status'] == 'running') 
+
+for i in range(math.floor( ( max - min ) / step)):
+    while processes() > max_processes :
+        pass
+    print(i, "out of", math.floor( ( max - min ) / step))
+    os.popen(
+            "python3 mandelbrotCopy.py {} {} {} {} {}"
+            .format(
+                i * step + min, 
+                maxReps,
+                transX,
+                transY,
+                "movie/" + str(i) + ".png"
+            )
+    )
